bc865bbbebf6745a9a2f427fcc543212cc8f47fa
Duke <duke@riseup.net>
2012-03-31 01:52:25 -0300

    fixed codemirror

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,4 +1,4 @@
 document.addEventListener("DOMContentLoaded", function(){
   var code_textarea = document.getElementById("code");
-  var CodeMirror = CodeMirror.fromTextArea(code_textarea, {mode: "javascript", theme:"monokai"});
+  var code_editor = CodeMirror.fromTextArea(code_textarea, {mode: "javascript", theme:"monokai"});
 }, false);
