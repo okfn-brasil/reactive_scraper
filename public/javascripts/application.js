2946b044d1f2f4513fa0d6bcaff5e1fbdc748d48
Duke <duke@riseup.net>
2012-03-31 15:50:56 -0300

    configure a better code_editor

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,6 +1,17 @@
 document.addEventListener("DOMContentLoaded", function(){
   var code_textarea = $("#code");
-  var code_editor = CodeMirror.fromTextArea(code_textarea[0], {mode: "javascript", theme:"monokai", value: code_textarea.val()});
+  var code_editor = CodeMirror.fromTextArea(code_textarea[0], {
+    mode: "javascript",
+    theme:"monokai",
+    value: code_textarea.val(),
+    lineNumbers: true,
+    gutter: true,
+    matchBrackets: true,
+    indentWithTabs:true,
+    indentUnit:4,
+    tabSize:4,
+    fixedGutter:true,
+  });
 
 
   var socket = io.connect(window.location.hostname);
