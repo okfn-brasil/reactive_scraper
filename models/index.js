fc061414396f06d6cfdf5fee8f8fa958f652bf65
Duke <duke@riseup.net>
2012-05-10 12:37:35 -0300

    fixed problem with result

--- models/index.js
+++ models/index.js
@@ -5,5 +5,4 @@ models.connection = null;
 models.configure = function(config){
   this.connection = config.connection;
   this.scraper = require("./scraper").init(this.connection);
-  this.result = require("./result").init(this.connection);
 }
